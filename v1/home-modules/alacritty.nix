af81f9059c726a8e9b74348053a3324c3d82b22a
anisbsalah <anisbensalah.89@gmail.com>
2024-02-09 02:57:29 +0100

    update alacritty config files

--- v1/home-modules/alacritty.nix
+++ v1/home-modules/alacritty.nix
@@ -23,36 +23,42 @@ in
           primary = {
             background = "0x383c4a";
             foreground = "0xd3dae3";
+            dim_foreground = "0xd3dae3";
+            bright_foreground = "0xd3dae3";
+          };
+          cursor = {
+            text = "0x383c4a";
+            cursor = "0xd3dae3";
           };
           normal = {
             black = "0x4b5164";
-            blue = "0x4877b1";
-            cyan = "0x5294e2";
-            green = "0x5ca75b";
-            magenta = "0xa660c3";
             red = "0xe14245";
-            white = "0xa9a9aa";
+            green = "0x5ca75b";
             yellow = "0xf6ab32";
+            blue = "0x4877b1";
+            magenta = "0xa660c3";
+            cyan = "0x5294e2";
+            white = "0xa9a9aa";
           };
           bright = {
             black = "0x616981";
-            blue = "0x8ca9bf";
-            cyan = "0x73c5e2";
-            green = "0xadd488";
-            magenta = "0xe2afec";
             red = "0xe16f7e";
-            white = "0xfcfcfc";
+            green = "0xadd488";
             yellow = "0xfdc35f";
+            blue = "0x8ca9bf";
+            magenta = "0xe2afec";
+            cyan = "0x73c5e2";
+            white = "0xfcfcfc";
           };
           dim = {
             black = "0x2c2f3a";
-            blue = "0x2b486b";
-            cyan = "0x1b668f";
-            green = "0x4b7c16";
-            magenta = "0x614a73";
             red = "0x783228";
-            white = "0x63686d";
+            green = "0x4b7c16";
             yellow = "0xb65619";
+            blue = "0x2b486b";
+            magenta = "0x614a73";
+            cyan = "0x1b668f";
+            white = "0x63686d";
           };
         };
 
